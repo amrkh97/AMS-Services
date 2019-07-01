2704eb2fbd800aa6cebb180a7b112b5ef8a83e4d
Mustafa <m.mufeed1996@gmail.com>
2019-07-01 13:02:57 +0200

    added add location serivce

---
+++ amo/src/main/java/Models/Locations/LocationResponse.java
@@ -0,0 +1,26 @@
+package Models.Locations;
+
+public class LocationResponse {
+	private int locationID;
+	private String responseMessage;
+	private String returnHex;
+	public int getLocationID() {
+		return locationID;
+	}
+	public void setLocationID(int locationID) {
+		this.locationID = locationID;
+	}
+	public String getResponseMessage() {
+		return responseMessage;
+	}
+	public void setResponseMessage(String responseMessage) {
+		this.responseMessage = responseMessage;
+	}
+	public String getReturnHex() {
+		return returnHex;
+	}
+	public void setReturnHex(String returnHex) {
+		this.returnHex = returnHex;
+	}
+
+}
