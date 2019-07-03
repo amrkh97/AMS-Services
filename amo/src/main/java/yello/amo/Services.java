79fad020720bba4a9c9edeeb44bbaa1737182b6a
amrkh97 <amrkh97@gmail.com>
2019-07-03 15:26:19 +0200

    Fixed Merge Error

--- amo/src/main/java/yello/amo/Services.java
+++ amo/src/main/java/yello/amo/Services.java
@@ -1,7 +1,7 @@
 package yello.amo;
 
 import java.util.ArrayList;
-
+import BLL.*;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,15 +9,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-// import org.json.JSONObject;
-
-import BLL.*;
-<<<<<<< HEAD
 import Models.ServerResponse;
-=======
 import Models.AmbulanceMap.AmbulanceMapModel;
->>>>>>> master
 import Models.AmbulanceVehicle.AmbulanceVehicleModel;
 import Models.Company.CompanyModel;
 import Models.Firebase.FBLocation.FBLocationEnum;
