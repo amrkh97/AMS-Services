8a260e1608ba6c23381448ef0ba4938fa24e363c
amrkh97 <amrkh97@gmail.com>
2019-07-05 10:08:21 +0200

    Added Missing functionalities

--- amo/src/main/java/BLL/EmployeeManager.java
+++ amo/src/main/java/BLL/EmployeeManager.java
@@ -7,36 +7,36 @@ import Models.Employee.EmployeeModel;
 
 public class EmployeeManager {
 
-	public static ArrayList<EmployeeModel> getAllParamedics() {
+	public static ArrayList<EmployeeModel> getAllParamedics(Integer superSSN) {
 		
-		return EmployeeDAL.getAllParamedics();
+		return EmployeeDAL.getAllParamedics(superSSN);
 	}
 	
-	public static ArrayList<EmployeeModel> getActiveParamedics() {
+	public static ArrayList<EmployeeModel> getActiveParamedics(Integer superSSN) {
 	
-		return EmployeeDAL.getActiveParamedics();
+		return EmployeeDAL.getActiveParamedics(superSSN);
 	}
 	
 	
-	public static ArrayList<EmployeeModel> getInActiveParamedics() {
+	public static ArrayList<EmployeeModel> getInActiveParamedics(Integer superSSN) {
 		
-		return EmployeeDAL.getInActiveParamedics();
+		return EmployeeDAL.getInActiveParamedics(superSSN);
 	}
 	
-	public static ArrayList<EmployeeModel> getAllDrivers() {
+	public static ArrayList<EmployeeModel> getAllDrivers(Integer superSSN) {
 		
-		return EmployeeDAL.getAllDrivers();
+		return EmployeeDAL.getAllDrivers(superSSN);
 	}
 	
-	public static ArrayList<EmployeeModel> getActiveDrivers() {
+	public static ArrayList<EmployeeModel> getActiveDrivers(Integer superSSN) {
 	
-		return EmployeeDAL.getActiveDrivers();
+		return EmployeeDAL.getActiveDrivers(superSSN);
 	}
 	
 	
-	public static ArrayList<EmployeeModel> getInActiveDrivers() {
+	public static ArrayList<EmployeeModel> getInActiveDrivers(Integer superSSN) {
 		
-		return EmployeeDAL.getInActiveDrivers();
+		return EmployeeDAL.getInActiveDrivers(superSSN);
 	}
 	
 }
