954b9046f7e76aa244b9d140ae1c866fd9c63f90
KamelMohsen <kamelmohsenkamel@gmail.com>
2019-07-10 16:19:22 +0200

    Added Output for current id

---
+++ amo/src/main/java/Models/ServerResponseIntOutput.java
@@ -0,0 +1,15 @@
+package Models;
+
+public class ServerResponseIntOutput extends ServerResponse{
+	 
+	private Integer Output;
+
+	public Integer getOutput() {
+		return Output;
+	}
+
+	public void setOutput(Integer output) {
+		Output = output;
+	} 
+
+}
