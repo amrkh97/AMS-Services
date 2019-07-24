de38c6251519994e5e5f6130515371804bfde92e
amrkh97 <amrkh97@gmail.com>
2019-07-24 16:55:40 +0200

    WIP: Incident Priority moved Connection

--- amo/src/main/java/BLL/IncidentPriorityManager.java
+++ amo/src/main/java/BLL/IncidentPriorityManager.java
@@ -1,11 +1,27 @@
 package BLL;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import DAL.IncidentPriorityDAL;
+import DB.DBManager;
 import Models.Priority.IncidentPriorityJson;
 
 public class IncidentPriorityManager {
 	public static IncidentPriorityJson getIncidentPriority() {
-		return IncidentPriorityDAL.getIncidentPriority();
+		Connection intermediateConnection = DBManager.getDBConn();
+		IncidentPriorityJson model = new IncidentPriorityJson();
+		try {
+			model = IncidentPriorityDAL.getIncidentPriority(intermediateConnection);
+		} finally {
+			try {
+				intermediateConnection.close();
+				System.out.println("Connection Closed");
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return model;
 	}
 
 }
